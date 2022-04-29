24b82940ce37dcbe333d577458203c4f957e822d
Luke Zhu <luke@observeinc.com>
2022-04-29 14:00:01 +0700

    add poller service account (#2)

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,17 @@
+output "project_id" {
+  description = "" // TODO:
+  value       = data.google_client_config.this.project
+}
+
 output "subscription_id" {
   description = <<EOF
     The id of the Pub/Sub subscription created by this module (example: observe-collection)".
   EOF
   value       = google_pubsub_subscription.this.name
+}
+
+output "service_account_key" {
+  description = "A service account key to be passed to the pollers for Pub/Sub and Cloud Monitoring"
+  value       = google_service_account_key.poller
+  sensitive   = true
 }
