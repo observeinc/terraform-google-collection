80076babe0703d617849ee294d6745853eca52e4
Luke Zhu <luke@observeinc.com>
2022-06-02 18:09:11 +0700

    feat: iteration 4 (#4)
    * update cloudfunction source
    
    * deploy cloud function code in "terraform apply"
    
    This means the user won't need to reference a bucket in our
    account.
    
    * use archive_file data source instead of resource
    
    * port 'gcpsetup' template to example directory
    
    * pre-commit
    
    * make README acceptable documentation
    
    * address tflint

--- versions.tf
+++ versions.tf
@@ -2,7 +2,8 @@ terraform {
   required_version = ">= 0.12.21"
 
   required_providers {
-    google = ">= 4.15"
-    time   = ">= 0.7.2"
+    archive = ">= 2.2.0"
+    google  = ">= 4.15"
+    time    = ">= 0.7.2"
   }
 }
