0adb4b0209fcaab3df362aead75c10ecfb03ec82
Colin Hutchinson <colin.hutchinson@observeinc.com>
2023-08-15 11:26:17 -0400

    release(cloud-function): bump the cloud function version

--- variables.tf
+++ variables.tf
@@ -140,7 +140,7 @@ variable "function_bucket" {
 variable "function_object" {
   description = "GCS object key of the Cloud Function source code zip file"
   type        = string
-  default     = "google-cloud-functions-v0.3.0-alpha.31.zip"
+  default     = "google-cloud-functions-v0.3.0.zip"
 }
 
 variable "function_available_memory_mb" {
