54d39c04df9dca0c55c8dfc1e05937b097171a41
Yasar Hussain <yasar@observeinc.com>
2023-08-16 10:58:44 -0700

    fix: add tf docs to examples

--- examples/service_account/main.tf
+++ examples/service_account/main.tf
@@ -5,18 +5,26 @@ resource "google_service_account" "this" {
   project     = var.project
 }
 
-resource "google_project_iam_member" "this" {
-  for_each = var.project_collection_roles
+###############
+#
+# Uncomment the first section for a service account that can deploy to a project
+# and uncomment the second section for folder collection.  If you are deploying to
+# a folder, you need to add the folder id to the service_account.auto.tfvars file as well.
+#
+################
 
-  project = var.project
-  role    = each.key
-  member  = "serviceAccount:${google_service_account.this.email}"
-}
+# resource "google_project_iam_member" "this" {
+#   for_each = var.project_collection_roles
 
-resource "google_folder_iam_member" "this" {
-  for_each = var.folder_collection_roles
+#   project = var.project
+#   role    = each.key
+#   member  = "serviceAccount:${google_service_account.this.email}"
+# }
 
-  folder = var.folder
-  role    = each.key
-  member  = "serviceAccount:${google_service_account.this.email}"
-}
+# resource "google_folder_iam_member" "this" {
+#   for_each = var.folder_collection_roles
+
+#   folder = var.folder
+#   role    = each.key
+#   member  = "serviceAccount:${google_service_account.this.email}"
+# }
